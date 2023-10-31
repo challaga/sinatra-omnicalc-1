38fed4198d30846fdba10c542cf0f13aadd96d43
challaga <146873313+challaga@users.noreply.github.com>
2023-10-31 01:40:35 +0000

    final

--- app.rb
+++ app.rb
@@ -7,3 +7,27 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  erb(:payment_results)
+end
